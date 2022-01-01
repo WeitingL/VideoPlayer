5c1c71fae1790f9fd72f7f9d57378a88947a44cf
WeitingL <ss890900@gmail.com>
2022-01-01 22:53:42 +0800

    Create the videoList with RecyclerView and show the uri infomation.

--- app/src/main/java/com/weiting/videoplayer/ui/videolist/VideoListFragment.kt
+++ app/src/main/java/com/weiting/videoplayer/ui/videolist/VideoListFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.navigation.NavGraphNavigator
 import androidx.navigation.fragment.findNavController
+import com.weiting.videoplayer.GlobalApplication
 import com.weiting.videoplayer.NavigationDirections
 import com.weiting.videoplayer.databinding.FragmentVideolistBinding
+import com.weiting.videoplayer.factory.VideoListViewModelFactory
 
 class VideoListFragment : Fragment() {
 
@@ -19,10 +23,15 @@ class VideoListFragment : Fragment() {
     ): View {
         val binding = FragmentVideolistBinding.inflate(inflater, container, false)
 
-        binding.test.setOnClickListener {
-            findNavController().navigate(NavigationDirections.actionGlobalMediaPlayerFragment())
+        val vmFactory = VideoListViewModelFactory(GlobalApplication.application.database)
+        val viewModel = ViewModelProvider(this, vmFactory).get(VideoListViewModel::class.java)
+        val adapter = VideoListAdapter()
+
+        viewModel.videoList.observe(viewLifecycleOwner){
+            adapter.submitList(it)
         }
 
+        binding.rvVideoList.adapter = adapter
         return binding.root
     }
 
--- app/src/main/java/com/weiting/videoplayer/ui/videolist/VideoListViewModel.kt
+++ app/src/main/java/com/weiting/videoplayer/ui/videolist/VideoListViewModel.kt
@@ -1,6 +1,19 @@
 package com.weiting.videoplayer.ui.videolist
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.weiting.videoplayer.data.VideoRepository
 
-class VideoListViewModel: ViewModel() {
+class VideoListViewModel(private val videoRepository: VideoRepository) : ViewModel() {
+
+    private val _videoList = MutableLiveData<List<String>>()
+    val videoList: LiveData<List<String>>
+        get() = _videoList
+
+    init {
+        _videoList.value = videoRepository.getAllVideo()
+    }
+
+
 }
